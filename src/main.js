b8bf72e6dea91a10382becabba315009734df339
keno80 <1211582428@qq.com>
2022-05-28 00:36:31 +0800

    add: 新增vuex

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router'
+import store from '@/store'
 
 import 'element-plus/theme-chalk/src/dark/css-vars.scss'
 import '@/styles/css-vars.scss'
 import 'uno.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
