17b8e72962814d43037263af74af0b126a0cd0b4
keno80 <1211582428@qq.com>
2022-06-07 23:30:28 +0800

    fix: 修改播放器模块布局

--- src/views/Home/components/Player.vue
+++ src/views/Home/components/Player.vue
@@ -4,11 +4,13 @@
     <div class="player">
       <div class="song">
         <p>Song Name</p>
+        <span>artist</span>
       </div>
+      <div class="divider" />
       <div class="controll">
-        <div class="i-mdi:play icon"></div>
+        <div class="i-mdi:skip-previous icon"></div>
+        <div class="i-mdi:motion-play-outline icon"></div>
         <div class="i-mdi:skip-next icon"></div>
-        <div class="i-mdi:volume-medium icon"></div>
       </div>
     </div>
   </div>
@@ -20,6 +22,7 @@
 .player_box {
   padding: 0 10px;
   display: flex;
+  font-family: 'Google Sans English';
 
   .album {
     width: 90px;
@@ -31,10 +34,27 @@
 
   .player {
     margin-left: 10px;
+    flex: auto;
+
+    p {
+      margin: 0 0;
+      padding-left: 4px;
+    }
+
+    span {
+      padding-left: 4px;
+      color: #e6e6e6;
+    }
+
+    .divider {
+      border-top: 1px solid rgba(255, 255, 255, 0.2);
+      margin: 10px 0;
+    }
 
     .controll {
       display: flex;
-      justify-content: center;
+      justify-content: space-between;
+      align-items: center;
 
       .icon {
         width: 1.8em;
