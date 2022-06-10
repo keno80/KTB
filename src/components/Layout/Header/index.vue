05619c101c784023d9f60fb7fc8bdd809e0bc2a8
keno80 <1211582428@qq.com>
2022-06-10 16:35:10 +0800

    fix: header重新布局

--- src/components/Layout/Header/index.vue
+++ src/components/Layout/Header/index.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="header">
-    <div class="left">
-      <!-- <button
-        :class="[isCollapse ? 'i-ep:expand' : 'i-ep:fold']"
-        class="svg"
-        @click="toggleSideCollapse()"
-      /> -->
-    </div>
+    <div class="left"></div>
     <div class="right">
-      <button class="i-mdi:shape svg"></button>
+      <div class="item">
+        <button class="i-mdi:shape svg"></button>
+      </div>
+      <div class="item">
+        <button class="i-mdi:home svg"></button>
+      </div>
     </div>
   </div>
 </template>
@@ -37,9 +36,31 @@ const toggleSideCollapse = () => {
   padding: 0 20px;
   justify-content: space-between;
 
+  .right {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .item {
+      padding: 8px;
+      background-color: var(--c-home-bg-color-2);
+      border-radius: 50%;
+      margin-left: 8px;
+      border: 1px solid transparent;
+      cursor: pointer;
+
+      &:hover {
+        box-shadow: inset 0px 0px 6px 0px var(--c-text-color),
+          0px 0px 6px 0 var(--c-text-color);
+        border: 1px solid var(--c-border-color-6);
+        transition: ease-in-out 0.2s;
+      }
+    }
+  }
+
   .svg {
     cursor: pointer;
-    color: #fff
+    color: #fff;
   }
 }
 </style>
