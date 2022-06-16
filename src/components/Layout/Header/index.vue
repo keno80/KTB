69ba6c889aa0ec539056b2105ef5fdc2d7ba9b7d
keno80 <1211582428@qq.com>
2022-06-16 23:52:36 +0800

    fix: 修改header布局

--- src/components/Layout/Header/index.vue
+++ src/components/Layout/Header/index.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="header">
     <div class="left">
-      <div class="item">
-        <span>KTB Tool Box </span>
+      <div class="left_item" v-if="mode === 'tools'">
+        <div class="item">
+          <button class="i-mdi:shape svg"></button>
+          <span class="item_text">前端技术</span>
+        </div>
       </div>
     </div>
     <div class="right">
-      <div class="item">
-        <button class="i-mdi:shape svg"></button>
-      </div>
-      <div class="item">
+      <div class="item" v-if="mode === 'tools'" @click="toHome">
         <button class="i-mdi:home svg"></button>
+      </div>
+      <div class="item" v-else @click="toTools">
+        <button class="i-mdi:shape svg"></button>
       </div>
     </div>
   </div>
@@ -19,35 +22,49 @@
 <script setup>
 import { computed } from 'vue'
 import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
 // import { toggleDark } from '@/composables'
 
+const router = useRouter()
 const store = useStore()
+
 const isCollapse = computed(() => store.state.app.isCollapse)
+const mode = computed(() => store.state.app.mode)
 
 const toggleSideCollapse = () => {
   isCollapse.value
     ? store.commit('app/toggleSideCollapse', false)
     : store.commit('app/toggleSideCollapse', true)
 }
+
+const toHome = () => {
+  router.push('/')
+  store.dispatch('app/setMode', 'home')
+}
+
+const toTools = () => {
+  router.push('/tools')
+  store.dispatch('app/setMode', 'tools')
+}
 </script>
 
 <style lang="scss" scoped>
 .header {
-  height: 50px;
+  height: 58px;
   // border-bottom: 1px solid var(--el-menu-border-color);
   display: flex;
   align-items: center;
   padding: 0 20px;
   justify-content: space-between;
 
-  .left,
+  .left_item,
   .right {
     display: flex;
     align-items: center;
     justify-content: center;
 
     .item {
-      padding: 8px;
+      padding: 6px;
       background-color: var(--c-home-bg-color-2);
       border-radius: 50%;
       margin-left: 8px;
@@ -67,12 +84,29 @@ const toggleSideCollapse = () => {
     }
   }
 
-  .left {
+  .left_item {
     margin-left: 0;
     margin-right: 8px;
 
     .item {
-      border-radius: 10px;
+      border-radius: 18px;
+      width: 20px;
+      transition: all ease-in-out 0.2s;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
+
+      &:hover {
+        width: 88px;
+      }
+
+      &:hover .item_text {
+        display: inline-block;
+      }
+
+      .item_text {
+        display: none;
+      }
     }
   }
 
