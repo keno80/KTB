9b16265df0161f5a97686f7e3628ca1b4fea1782
keno80 <1211582428@qq.com>
2022-06-28 18:26:11 +0800

    feat: 添加tools工具集合

---
+++ src/utils/items.js
@@ -0,0 +1,428 @@
+export const list = [
+  {
+    name: '技术文档',
+    index: 0,
+    items: [
+      {
+        icon: 'https://developer.mozilla.org/favicon-48x48.cbbd161b.png',
+        title: 'MDN Web Docs',
+        description:
+          'MDN Web Docs 是一个提供 Web 技术和促进 Web 技术软件的不断发展的学习平台',
+        url: 'https://developer.mozilla.org/zh-CN/',
+      },
+      {
+        icon: 'https://www.w3school.com.cn/ui2019/logo-48-red.png',
+        title: 'W3 Schools',
+        description: '在 W3School，你可以找到你所需要的所有的网站建设教程。',
+        url: 'https://www.w3school.com.cn/',
+      },
+      {
+        icon: 'https://static.runoob.com/images/favicon.ico',
+        title: '菜鸟教程',
+        description: '学的不仅是技术，更是梦想！',
+        url: 'https://www.runoob.com/',
+      },
+      {
+        icon: 'https://cn.vuejs.org/images/icons/favicon-96x96.png',
+        title: 'Vue.js',
+        description:
+          'Vue是一套用于构建用户界面的渐进式框架。与其它大型框架不同的是，Vue 被设计为可以自底向上逐层应用。Vue 的核心库只关注视图层，不仅易于上手，还便于与第三方库或既有项目整合。',
+        url: 'https://www.runoob.com/',
+      },
+      {
+        icon: 'https://zh-hans.reactjs.org/favicon.ico',
+        title: 'React.js',
+        description: '用于构建用户界面的 JavaScript 库，起源于Facebook内部',
+        url: 'https://zh-hans.reactjs.org/',
+      },
+      {
+        icon: 'https://www.angularjs.net.cn/Application/Home/View/Public/img/favicon.ico',
+        title: 'Angular.js',
+        description:
+          'AngularJS是一个开发动态Web应用的框架。它让你可以使用HTML作为模板语言并且可以通过扩展的HTML语法来使应用组件更加清晰和简洁。',
+        url: 'https://www.angularjs.net.cn/',
+      },
+      {
+        icon: 'https://www.jquery123.com/assets/images/favicon.ico',
+        title: 'jQuery 中文网',
+        description:
+          'jQuery 是一个高效、精简并且功能丰富的 JavaScript 工具库。它提供的 API 易于使用且兼容众多浏览器，这让诸如 HTML 文档遍历和操作、事件处理、动画和 Ajax 操作更加简单。',
+        url: 'https://www.jquery123.com/',
+      },
+      {
+        icon: 'https://www.nuxtjs.cn/favicon.ico',
+        title: 'Nuxt.js',
+        description:
+          'Nuxt.js 是一个基于 Vue.js 的通用应用框架。通过对客户端/服务端基础架构的抽象组织，Nuxt.js 主要关注的是应用的 UI 渲染。',
+        url: 'https://www.nuxtjs.cn/',
+      },
+      {
+        icon: 'https://www.nextjs.cn/static/favicon/favicon-32x32.png',
+        title: 'Next.js',
+        description:
+          'Next.js 为您提供生产环境所需的所有功能以及最佳的开发体验：包括静态及服务器端融合渲染、 支持 TypeScript、智能化打包、 路由预取等功能 无需任何配置。',
+        url: 'https://www.nextjs.cn/',
+      },
+      {
+        icon: 'https://www.sveltejs.cn/favicon.png',
+        title: 'Svelte',
+        description:
+          'Svelte 是一种全新的构建用户界面的方法。传统框架如 React 和 Vue 在浏览器中需要做大量的工作，而 Svelte 将这些工作放到构建应用程序的编译阶段来处理。',
+        url: 'https://www.sveltejs.cn/',
+      },
+      {
+        icon: 'https://www.eggjs.org/favicon.png',
+        title: 'Egg.js',
+        description:
+          'Egg.js 为企业级框架和应用而生，专注于提供 Web 开发的核心功能和一套灵活可扩展的插件机制',
+        url: 'https://www.eggjs.org/zh-CN',
+      },
+      {
+        icon: 'https://expressjs.com/images/favicon.png',
+        title: 'Express.js',
+        description: '高度包容、快速而极简的 Node.js Web 框架',
+        url: 'https://expressjs.com/zh-cn/',
+      },
+      {
+        icon: 'https://thinkjs.org/static/img/favicon.png',
+        title: 'Think.js',
+        description:
+          'ThinkJS 是一款面向未来开发的 Node.js 框架，整合了大量的项目最佳实践，让企业级开发变得更简单、高效。从 3.0 开始，框架底层基于 Koa 2.x 实现，兼容 Koa 的所有功能',
+        url: 'https://thinkjs.org/',
+      },
+      {
+        icon: 'https://less.bootcss.com/public/ico/favicon.ico',
+        title: 'Less 中文网',
+        description:
+          'Less是一门向后兼容的 CSS 扩展语言。包含了 Less 语言以及利用 JavaScript 开发的用于将 Less 样式转换成 CSS 样式的 Less.js 工具。',
+        url: 'https://thinkjs.org/',
+      },
+      {
+        icon: 'https://www.sass.hk/favicon.ico',
+        title: 'Sass 中文网',
+        description: '世界上最成熟、最稳定、最强大的专业级CSS扩展语言！',
+        url: 'https://www.sass.hk/',
+      },
+      {
+        icon: 'https://www.stylus-lang.cn/favicon.ico',
+        title: 'Stylus',
+        description: '富于表现力、动态的、健壮的 CSS',
+        url: 'https://www.stylus-lang.cn/',
+      },
+      {
+        icon: 'https://www.postcss.com.cn/logo.11c14285.svg',
+        title: 'PostCss 中文网',
+        description: 'PostCSS是一个用 JavaScript 工具和插件转换 CSS 代码的工具',
+        url: 'https://www.postcss.com.cn/',
+      },
+      {
+        icon: 'https://www.tailwindcss.cn/favicon-32x32.png',
+        title: 'TailWind CSS',
+        description:
+          'Tailwind CSS 是一个功能类优先的 CSS 框架，它集成了诸如 flex, pt-4, text-center 和 rotate-90 这样的的类，它们能直接在脚本标记语言中组合起来，构建出任何设计。',
+        url: 'https://www.tailwindcss.cn/',
+      },
+      {
+        icon: 'https://cn.windicss.org/assets/logo.svg',
+        title: 'Windi CSS',
+        description: 'Windi CSS 是下一代工具优先的 CSS 框架。',
+        url: 'https://cn.windicss.org/',
+      },
+      {
+        icon: 'https://uno.antfu.me/favicon.svg',
+        title: 'UnoCSS',
+        description:
+          'UnoCSS 是一个引擎，而非一款框架，因为它并未提供核心工具类，所有功能可以通过预设和内联配置提供。',
+        url: 'https://uno.antfu.me/',
+      },
+      {
+        icon: 'https://cn.eslint.org/img/favicon.512x512.png',
+        title: 'ESLint',
+        description: '可组装的JavaScript和JSX检查工具',
+        url: 'https://cn.eslint.org/',
+      },
+      {
+        icon: 'https://vuex.vuejs.org/logo.png',
+        title: 'Vuex',
+        description:
+          'Vuex 是一个专为 Vue.js 应用程序开发的状态管理模式 + 库。它采用集中式存储管理应用的所有组件的状态，并以相应的规则保证状态以一种可预测的方式发生变化。',
+        url: 'https://vuex.vuejs.org/zh/',
+      },
+      {
+        icon: 'http://cn.redux.js.org/img/favicon/favicon.ico',
+        title: 'Redux',
+        description: 'JS 应用的状态容器，提供可预测的状态管理',
+        url: 'http://cn.redux.js.org/',
+      },
+      {
+        icon: 'https://pinia.vuejs.org/logo.svg',
+        title: 'Pinia',
+        description:
+          'Pinia 是 Vue.js 的轻量级状态管理库。它使用 Vue 3 中的新反应系统来构建一个直观且完全类型化的状态管理库。',
+        url: 'https://pinia.vuejs.org/',
+      },
+      {
+        icon: 'https://cn.vitejs.dev/logo.svg',
+        title: 'Vite',
+        description:
+          'Vite是一种新型前端构建工具，能够显著提升前端开发体验。Vite 意在提供开箱即用的配置，同时它的 插件 API 和 JavaScript API 带来了高度的可扩展性，并有完整的类型支持。',
+        url: 'https://cn.vitejs.dev/',
+      },
+    ],
+  },
+  {
+    name: 'UI 框架',
+    index: 1,
+    items: [
+      {
+        icon: 'https://fastly.jsdelivr.net/npm/@bootcss/www.bootcss.com@0.0.60/dist/ico/favicon.ico',
+        title: 'Bootstrap 中文网',
+        description: '简洁、直观、强悍的前端开发框架，让web开发更迅速、简单。',
+        url: 'https://www.bootcss.com/',
+      },
+      {
+        icon: 'https://element.eleme.cn/favicon.ico',
+        title: 'Element',
+        description:
+          '一套为开发者、设计师和产品经理准备的基于 Vue 2.0 的桌面端组件库',
+        url: 'https://element.eleme.cn/#/zh-CN',
+      },
+      {
+        icon: 'https://element-plus.org/images/element-plus-logo-small.svg',
+        title: 'Element Plus',
+        description: '基于 Vue 3，面向设计师和开发者的组件库',
+        url: 'https://www.bootcss.com/',
+      },
+      {
+        icon: 'https://qn.antdv.com/favicon.ico',
+        title: 'Ant Design Vue',
+        description:
+          '适用于Vue。为 Web 应用提供了丰富的基础 UI 组件，持续探索企业级应用的最佳 UI 实践。',
+        url: 'https://2x.antdv.com/docs/vue/introduce-cn',
+      },
+      {
+        icon: 'https://gw.alipayobjects.com/zos/rmsportal/rlpTLlbMzTNYuZGGCVYM.png',
+        title: 'Ant Design React',
+        description:
+          '适用于React。为 Web 应用提供了丰富的基础 UI 组件，持续探索企业级应用的最佳 UI 实践。',
+        url: 'https://ant.design/index-cn',
+      },
+      {
+        icon: 'https://next.vuetifyjs.com/favicon.ico',
+        title: 'Vuetify',
+        description:
+          'Vuetify 是一个带有精美手工材质组件的 Vue UI 库。 无需任何设计技能——创建令人惊叹的应用程序所需的一切都触手可及。',
+        url: 'https://next.vuetifyjs.com/en/',
+      },
+      {
+        icon: 'https://cdn.quasar.dev/logo-v2/favicon/favicon-96x96.png',
+        title: 'Quasar',
+        description:
+          'Quasar是 MIT 许可的基于 Vue.js 的开源框架，它允许您作为 Web 开发人员快速创建多种风格的响应式++网站/应用程序',
+        url: 'https://quasar.dev/',
+      },
+      {
+        icon: 'https://shoelace.style/assets/images/logo.svg',
+        title: 'Shoelace',
+        description: '具有前瞻性的 Web 组件库',
+        url: 'https://shoelace.style/',
+      },
+      {
+        icon: 'https://www.naiveui.com/assets/naivelogo.93278402.svg',
+        title: 'Naive UI',
+        description:
+          '一个 Vue 3 组件库比较完整，主题可调，使用 TypeScript，有点意思',
+        url: 'https://www.naiveui.com/zh-CN/os-theme',
+      },
+      {
+        icon: 'https://file.iviewui.com/view-design-logo.png',
+        title: 'iView',
+        description:
+          '基于 Vue.js 3 的企业级 UI 组件库和前端解决方案，为数万开发者服务。',
+        url: 'https://www.iviewui.com/',
+      },
+      {
+        icon: 'https://lf9-static.semi.design/obj/semi-tos/images/favicon.ico',
+        title: 'Semi Design',
+        description:
+          '由字节跳动抖音前端与 UED 团队设计、开发并维护，包含设计语言、React 组件、主题等开箱即用的中后台解决方案，帮助设计师与开发者打造高质量产品。',
+        url: 'https://semi.design/zh-CN/',
+      },
+      {
+        icon: 'https://taro-ui.jd.com/favicons/favicon.png',
+        title: 'Taro UI',
+        description: '一套基于 Taro 框架开发的多端 UI 组件库',
+        url: 'https://taro-ui.jd.com/#/',
+      },
+      {
+        icon: 'https://fastly.jsdelivr.net/npm/@vant/assets/logo.png',
+        title: 'Vant UI',
+        description:
+          '一个轻量、可靠的移动端组件库。目前 Vant 官方提供了 Vue 2 版本、Vue 3 版本和微信小程序版本，并由社区团队维护 React 版本和支付宝小程序版本。',
+        url: 'https://vant-contrib.gitee.io/vant/#/zh-CN/',
+      },
+      {
+        icon: 'https://img14.360buyimg.com/imagetools/jfs/t1/167902/2/8762/791358/603742d7E9b4275e3/e09d8f9a8bf4c0ef.png',
+        title: 'Nut UI',
+        description: '一个京东风格的轻量级移动端 Vue 组件库',
+        url: 'https://nutui.jd.com/3x/#/',
+      },
+      {
+        icon: 'https://layuion.com/favicon.ico',
+        title: 'LayUI',
+        description:
+          '开源模块化前端 UI 组件库基于浏览器端原生态模式，面向全层次的前后端开发者，易上手且开源免费的 Web 界面组件库',
+        url: 'https://layuion.com/',
+      },
+      {
+        icon: 'https://weui.io/favicon.ico',
+        title: 'WeUI',
+        description:
+          'WeUI 是一套同微信原生视觉体验一致的基础样式库，由微信官方设计团队为微信 Web 开发量身设计，可以令用户的使用感知更加统一。',
+        url: 'https://github.com/Tencent/weui/blob/master/README_cn.md',
+      },
+      {
+        icon: 'https://www.openzui.com/docs/favicon.ico',
+        title: 'ZUI',
+        description:
+          '一个基于 Bootstrap 深度定制开源前端实践方案，帮助你快速构建现代跨屏应用。',
+        url: 'https://www.openzui.com/#/',
+      },
+      {
+        icon: 'https://unpkg.byted-static.com/latest/byted/arco-config/assets/favicon.ico',
+        title: 'Arco Design',
+        description: '字节跳动出品的企业级设计系统',
+        url: 'https://arco.design/',
+      },
+      {
+        icon: 'https://i.loli.net/2018/04/06/5ac798bcb3f46.png',
+        title: 'VUX',
+        description: '一款移动端UI组件库',
+        url: 'https://vux.li/',
+      },
+      {
+        icon: 'https://l-ui.com/favicon.ico',
+        title: 'LuLu UI',
+        description: 'LuLu UI 是阅文集团荣誉出品的前端 UI 组件库。',
+        url: 'https://l-ui.com/',
+      },
+      {
+        icon: 'https://vuestic.dev/apple-touch-icon.png',
+        title: 'Vuestic UI',
+        description: '一个基于Vue3的UI 组件库',
+        url: 'https://vuestic.dev/',
+      },
+      {
+        icon: 'https://webapp.didistatic.com/static/webapp/shield/Cube-UI_logo.ico',
+        title: 'Cube UI',
+        description:
+          'cube-ui 是由滴滴内部组件库精简提炼而来,基于 Vue.js 实现的精致移动端组件库。',
+        url: 'https://didi.github.io/cube-ui/#/zh-CN',
+      },
+      {
+        icon: 'https://buefy.org/static/img/icons/apple-touch-icon.png',
+        title: 'Buefy',
+        description:
+          '基于 Bulma 和 Vue.js 的轻量级UI组件，它提供了即装即用的轻量级组件',
+        url: 'https://buefy.org/',
+      },
+      {
+        icon: 'https://at-ui.github.io/at-ui/favicons/apple-touch-icon-114x114.png',
+        title: 'AT UI',
+        description: '由凹凸实验室出品的一款轻量级、模块化的前端 UI 组件库',
+        url: 'https://at-ui.github.io/at-ui/#/zh',
+      },
+      {
+        icon: 'https://static.tdesign.tencent.com/favicon.ico',
+        title: 'TDesign',
+        description:
+          'TDesign 是腾讯开源的企业级设计体系，提供了统一设计价值观，一致的设计语言和视觉风格，和开箱即用的 UI 组件库。',
+        url: 'https://tdesign.tencent.com/',
+      },
+    ],
+  },
+  {
+    name: '前端插件',
+    index: 2,
+    items: [
+      {
+        icon: 'https://www.tiny.cloud/favicon-32x32.png',
+        title: 'TinyMCE',
+        description:
+          'TinyMCE 让您完全控制富文本编辑。要么通过 API 创建完全定制的体验，要么利用开箱即用的企业级编辑器来构建您的下一代 Web 应用程序。',
+        url: 'https://www.tiny.cloud/',
+      },
+      {
+        icon: 'https://lodash.com/icons/apple-touch-180x180.png',
+        title: 'Lodash',
+        description: '提供模块化、性能和附加功能的现代 JavaScript 实用程序库',
+        url: 'https://lodash.com/',
+      },
+      {
+        icon: 'https://fastly.jsdelivr.net/gh/apache/echarts-website@asf-site/zh/images/favicon.png?_v_=20200710_1',
+        title: 'Echarts',
+        description: '一个基于 JavaScript 的开源可视化图表库',
+        url: 'https://echarts.apache.org/zh/index.html',
+      },
+      {
+        icon: 'https://dayjs.gitee.io/img/favicon.ico',
+        title: 'Day.js',
+        description:
+          'Day.js 是一个轻量的处理时间和日期的 JavaScript 库，和 Moment.js 的 API 设计保持完全一样。',
+        url: 'https://dayjs.gitee.io/zh-CN/',
+      },
+      {
+        icon: 'https://momentjs.bootcss.com/static/img/moment-favicon.png',
+        title: 'Moment.js',
+        description: '解析、校验、操作、显示日期和时间的 JavaScript 工具库。',
+        url: 'https://momentjs.bootcss.com/',
+      },
+      {
+        icon: 'https://animejs.com/documentation/assets/img/apple-touch-icon.png',
+        title: 'Anime.js',
+        description:
+          'Anime.js是一个轻量级的 JavaScript 动画库，具有简单但功能强大的 API。 它适用于 CSS 属性、SVG、DOM 属性和 JavaScript 对象。',
+        url: 'https://animejs.com/',
+      },
+      {
+        icon: 'https://www.chartjs.org/favicon.ico',
+        title: 'Chart.js',
+        description: '为设计人员和开发人员提供简单而灵活的 JavaScript 图表',
+        url: 'https://www.chartjs.org/',
+      },
+      {
+        icon: 'https://sarcadass.github.io/granim.js/favicon.ico',
+        title: 'Granim.js',
+        description: '帮助您创建流畅的交互式渐变动画',
+        url: 'https://www.chartjs.org/',
+      },
+      {
+        icon: 'https://popper.js.org/icons/icon-144x144.png?v=e8966dab7a9bda34fd13edd5fb997ff2',
+        title: 'Popper.js',
+        description:
+          '使放置弹出式菜单，下拉菜单，工具提示和其他上下文元素（更靠近按钮或其他类似元素）更容易。',
+        url: 'https://popper.js.org/',
+      },
+      {
+        icon: 'https://threejs.org/files/favicon.ico',
+        title: 'Three.js',
+        description:
+          'Three.js是一个跨浏览器的脚本，使用JavaScript函数库或API来在网页浏览器中创建和展示动画的三维计算机图形。Three.js使用WebGL。',
+        url: 'https://threejs.org',
+      },
+      {
+        icon: 'https://vocajs.com/images/favicons/apple-touch-icon-144x144.png',
+        title: 'Voca.js',
+        description:
+          'Voca是一个用于处理字符串的优秀 JS 库。我们可以使用它来操作字符串，如更改大小写、修剪、pad、slugify、latinise、截断、转义等。',
+        url: 'https://vocajs.com/',
+      },
+      {
+        icon: 'https://apexcharts.com/wp-content/themes/apexcharts/favicon.ico',
+        title: 'ApexCharts',
+        description: '一个现代和交互式开源图表库',
+        url: 'https://apexcharts.com/',
+      },
+    ],
+  },
+]
